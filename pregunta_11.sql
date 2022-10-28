137ba2dcd78f2adad142cc8c6235eea09795841c
AlfredoVergara10 <112211579+AlfredoVergara10@users.noreply.github.com>
2022-10-28 18:59:26 -0500

    Correción

--- pregunta_11.sql
+++ pregunta_11.sql
@@ -39,4 +39,4 @@
 --  >>> Escriba su codigo a partir de este punto <<<
 -- 
 
-SELECT strftime("%Y", c14) AS YEAR, COUNT(*) AS CANT FROM tbl1 WHERE YEAR = "2018";
+SELECT strftime('%Y', c14) AS YEAR, COUNT(*) AS CANT FROM tbl1 WHERE YEAR = '2018';
